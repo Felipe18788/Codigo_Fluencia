87c445f1d9abf3e870907ed7e10718a5a45268ee
Felipe <felipececiliano99@gmail.com>
2025-02-20 10:07:32 -0300

    Update codigofluencia_mk1.py
    mark2

--- codigofluencia_mk1.py
+++ codigofluencia_mk1.py
@@ -1,6 +1,7 @@
 from scipy.stats import linregress
 import numpy as np
 
+stress_number = float(input("Qual o valor de tensão? "))
 temperature_number = int(input("Quantas temperaturas adicionar? "))
 straintime_number = int(input("Quantos valores de deformação e tempo adicionar? "))
 
@@ -20,24 +21,21 @@ for i in range(temperature_number):
 
         dados["deformação"].append(strain)
         dados["tempo"].append(time)
+print()
 
-print(f"Temperaturas: ", dados["temperatura"])
-print(f"Deformações: ", dados["deformação"])
-print(f"Tempos: ", dados["tempo"])
-
+#Cálculo dos valores das taxas de deformação 
 strain_rate = []
 for k in range(0,len(dados["deformação"]),straintime_number): 
     lin_strain_v = dados["deformação"][k:k+straintime_number]
     lin_time_v = dados["tempo"][k:k+straintime_number]
-    print(lin_strain_v)
-    print(lin_time_v)
 
     strain_rate_value, intercept, r_value, p_value, std_err = linregress(lin_time_v, lin_strain_v)
     
     strain_rate.append(strain_rate_value)
 
-print(f"Valores de taxa de deformação: {strain_rate}") 
+print(f"Valores das taxas de deformação: {strain_rate}\n") 
 
+#Cálculo do parâmetro "Q"
 temp = np.array(dados["temperatura"])+273
 
 lntaxa = np.log(strain_rate)
@@ -45,8 +43,5 @@ inversoT = 1/(temp)
 
 slope, intercept, r_value, p_value, std_err = linregress(inversoT,lntaxa)
 
-print(f"Valor de inclinação para a curva 1/T x ln(strain): {slope}")
-print(intercept)
-
 Q = (-8.31)*slope
-print(f"O valor de Q é de: {Q}")
+print(f"O valor de Q é de: {Q: .2f}\n")
